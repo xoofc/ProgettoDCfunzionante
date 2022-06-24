a88115b85ef415f35824683d78fe3b09073c9b26
EmanueleEDG <emanuele10napoli@gmail.com>
2022-06-24 14:51:29 +0200

    Create altre 2 nuovi classi nemico Sahuagin,Drago

---
+++ src/main/java/model/enemies/Sahuagin.java
@@ -0,0 +1,93 @@
+package model.enemies;
+
+public class Sahuagin extends Enemy{
+    private String name = "Sahuagin";
+    private long healt = 22;
+    private long damage;
+
+    public Sahuagin() {
+        livelMin = 25;
+        livelMax = 30;
+    }
+
+    private boolean hasStatus = false;
+
+    private boolean isFrozen = false;
+    @Override
+    public boolean getIsFrozen() {
+        return isFrozen;
+    }
+
+    @Override
+    public void setFrozen(boolean frozen) {
+        this.isFrozen = frozen;
+    }
+
+    public boolean getHasStatus() {
+        return hasStatus;
+    }
+    public void setHasStatus(boolean hasStatus) {
+        this.hasStatus = hasStatus;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getHealt() {
+        return healt;
+    }
+
+    @Override
+    public void setHealt(long healt) {
+        this.healt = healt;
+    }
+
+    public void setHealt(int healt) {
+        this.healt = healt;
+    }
+
+    public long getDamage() {
+        return damage;
+    }
+
+    @Override
+    public void setDamage(long damage) {
+
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public void doAttack() {
+
+    }
+
+    @Override
+    public long doDefend() {
+        return 0;
+    }
+
+    @Override
+    public void doSpecialAbility() {
+
+    }
+
+    public long setDamageOutput() {
+        return Math.round(Math.random() * (4 - 3) + 1);
+    }
+
+    public boolean isAlive() {
+        if (this.getHealt()>0) {
+            return true;
+        } else if (this.getHealt() <= 0) {
+            return false;
+        }
+        return false;
+    }
+}
